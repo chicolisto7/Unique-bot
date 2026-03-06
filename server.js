6b94ea297f9761d73ac3345856c05c56b04e2afd
chicolisto7 <rahimramadhanidaudi@gmail.com>
2026-03-06 10:14:04 +0300

    Refactor server.js

--- server.js
+++ server.js
@@ -1,34 +1,31 @@
-const express = require("express");
-const path = require("path");
-const cors = require("cors");
+const express = require("express")
+const path = require("path")
+const cors = require("cors")
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-// Serve static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname,"public")))
 
-// Homepage route
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.get("/", (req,res)=>{
+res.sendFile(path.join(__dirname,"public","index.html"))
+})
 
-// Pair code API mfano
-app.get("/pair", (req, res) => {
+app.get("/pair", (req,res)=>{
 
-    const code = Math.floor(100000 + Math.random() * 900000);
+let code = Math.floor(100000 + Math.random()*900000)
 
-    res.json({
-        status: true,
-        pair_code: code
-    });
+res.json({
+status:true,
+pair_code:code
+})
 
-});
+})
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT,()=>{
+console.log("UNIQUE BOT server running")
+})
