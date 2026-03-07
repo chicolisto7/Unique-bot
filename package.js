3efde97e44d01551315b3e943900bf9b7de63020
chicolisto7 <rahimramadhanidaudi@gmail.com>
2026-03-07 17:09:58 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "unique-bot",
+  "version": "2.2.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.6.0",
+    "express": "^4.18.2",
+    "pino": "^8.17.0"
+  }
+}
