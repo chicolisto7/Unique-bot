a178d771c20b5202842690cdbb223332fd9c7b82
chicolisto7 <rahimramadhanidaudi@gmail.com>
2026-03-08 15:53:17 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 {
   "name": "unique-bot",
-  "version": "2.2.0",
+  "version": "3.0.0",
   "main": "server.js",
   "scripts": {
     "start": "node server.js"
   },
   "dependencies": {
-    "@whiskeysockets/baileys": "^6.6.0",
+    "@whiskeysockets/baileys": "^6.7.5",
     "express": "^4.18.2",
     "pino": "^8.17.0"
   }
-      }
+}
